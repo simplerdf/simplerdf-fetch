f4a3d4e2c523d89719239f61a11dd5991eac06a9
bergi <bergi@axolotlfarm.org>
2017-11-16 21:45:47 +0100

    moved lite code to a separate package

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 const rdfFetch = require('rdf-fetch')
-const simpleFetchLite = require('./lite')
+const SimpleFetchLite = require('simplerdf-fetch-lite')
 
-function simpleFetch (url, options) {
-  options = options || {}
-  options.rdfFetch = options.rdfFetch || simpleFetch.defaults.rdfFetch
-  options.formats = options.formats || rdfFetch.defaults.formats
-  options.context = options.context || simpleFetch.defaults.context
+class SimpleFetch extends SimpleFetchLite {
+  init (context, iri, graph, options) {
+    this._options.rdfFetch = options.rdfFetch || SimpleFetch.defaults.rdfFetch
+  }
 
-  return simpleFetchLite(url, options)
+  fetch (url, options) {
+    options = options || {}
+    options.simpleFactory = this.create
+    options.context = options.context || this.context()
+    options.rdfFetch = options.rdfFetch || this._options.rdfFetch
+
+    return SimpleFetch.fetch(url, options)
+  }
+
+  static fetch (url, options) {
+    options = options || {}
+    options.rdfFetch = options.rdfFetch || SimpleFetch.defaults.rdfFetch
+    options.formats = options.formats || rdfFetch.defaults.formats
+    options.context = options.context || SimpleFetch.defaults.context
+
+    return SimpleFetchLite.fetch(url, options)
+  }
 }
 
-simpleFetch.defaults = {
+SimpleFetch.defaults = {
   rdfFetch: rdfFetch
 }
 
-module.exports = simpleFetch
+module.exports = SimpleFetch
--- test/test.js
+++ test/test.js
@@ -1,243 +1,19 @@
 /* global describe, it */
 
 const assert = require('assert')
-const formats = require('rdf-fetch').defaults.formats
 const nock = require('nock')
 const rdf = require('rdf-ext')
-const simpleFetch = require('..')
-const simpleFetchLite = require('../lite')
-const SimpleRDF = require('simplerdf-core')
+const Simple = require('simplerdf-core')
+const SimpleFetch = require('..')
 
 describe('simplerdf-fetch', () => {
-  describe('lite', () => {
-    it('should be a function', () => {
-      assert.equal(typeof simpleFetchLite, 'function')
-    })
-
-    it('should have a defaults object', () => {
-      assert.equal(typeof simpleFetchLite.defaults, 'object')
-    })
-
-    it('should return a Promise object', () => {
-      let result = simpleFetchLite('http://example.org/', {
-        formats: {
-          parsers: {
-            list: () => {
-              return []
-            }
-          }
-        }
-      })
-
-      assert.equal(typeof result, 'object')
-      assert.equal(typeof result.then, 'function')
-    })
-
-    it('should use the rdfFetch given in options', () => {
-      let touched = false
-
-      function touch () {
-        touched = true
-
-        return Promise.resolve({})
-      }
-
-      return simpleFetchLite(null, {rdfFetch: touch}).then(() => {
-        assert.equal(touched, true)
-      })
-    })
-
-    it('should use the rdfFetch given in defaults', () => {
-      let touched = false
-
-      function touch () {
-        touched = true
-
-        return Promise.resolve({})
-      }
-
-      const defaultRdfFetch = simpleFetchLite.defaults.rdfFetch
-
-      simpleFetchLite.defaults.rdfFetch = touch
-
-      return simpleFetchLite().then(() => {
-        assert.equal(touched, true)
-
-        simpleFetchLite.defaults.rdfFetch = defaultRdfFetch
-      })
-    })
-
-    it('should send the SimpleRDF object given on options.body', () => {
-      nock('http://example.org')
-        .post('/send-body')
-        .reply(200, function (url, body) {
-          assert.equal(body.trim(), '<http://example.org/subject> <http://example.org/predicate> "object" .')
-        })
-
-      const customFormats = {
-        parsers: new rdf.Parsers({
-          'application/n-triples': formats.parsers['application/n-triples']
-        }),
-        serializers: new rdf.Serializers({
-          'application/n-triples': formats.serializers['application/n-triples']
-        })
-      }
-
-      let simple = new SimpleRDF({predicate: 'http://example.org/predicate'}, 'http://example.org/subject')
-
-      simple.predicate = 'object'
-
-      return simpleFetchLite('http://example.org/send-body', {
-        formats: customFormats,
-        method: 'post',
-        body: simple
-      })
-    })
-
-    it('should receive the SimpleRDF object', () => {
-      nock('http://example.org')
-        .get('/receive-body')
-        .reply(200, function (url, body) {
-          return [200, '<http://example.org/subject> <http://example.org/predicate> "object" .\n', {
-            'Content-Type': 'application/n-triples'
-          }]
-        })
-
-      const customFormats = {
-        parsers: new rdf.Parsers({
-          'application/n-triples': formats.parsers['application/n-triples']
-        }),
-        serializers: new rdf.Serializers({
-          'application/n-triples': formats.serializers['application/n-triples']
-        })
-      }
-
-      return simpleFetchLite('http://example.org/receive-body', {formats: customFormats}).then((res) => {
-        const graphString = res.simple.graph().toString().trim()
-
-        assert.equal(graphString, '<http://example.org/subject> <http://example.org/predicate> "object" .')
-      })
-    })
-
-    it('should use the context given in options for the received SimpleRDF object', () => {
-      nock('http://example.org')
-        .get('/context-options')
-        .reply(200, function (url, body) {
-          return [200, '<http://example.org/subject> <http://example.org/predicate> "object" .\n', {
-            'Content-Type': 'application/n-triples'
-          }]
-        })
-
-      const customFormats = {
-        parsers: new rdf.Parsers({
-          'application/n-triples': formats.parsers['application/n-triples']
-        }),
-        serializers: new rdf.Serializers({
-          'application/n-triples': formats.serializers['application/n-triples']
-        })
-      }
-
-      const options = {
-        formats: customFormats,
-        context: {
-          predicate: 'http://example.org/predicate'
-        }
-      }
-
-      return simpleFetchLite('http://example.org/context-options', options).then((res) => {
-        assert.deepEqual(res.simple.context()._json, {predicate: 'http://example.org/predicate'})
-      })
-    })
-
-    it('should use the context given in defaults for the received SimpleRDF object', () => {
-      nock('http://example.org')
-        .get('/context-options')
-        .reply(200, function (url, body) {
-          return [200, '<http://example.org/subject> <http://example.org/predicate> "object" .\n', {
-            'Content-Type': 'application/n-triples'
-          }]
-        })
-
-      const customFormats = {
-        parsers: new rdf.Parsers({
-          'application/n-triples': formats.parsers['application/n-triples']
-        }),
-        serializers: new rdf.Serializers({
-          'application/n-triples': formats.serializers['application/n-triples']
-        })
-      }
-
-      simpleFetchLite.defaults.context = {predicate: 'http://example.org/predicate'}
-
-      return simpleFetchLite('http://example.org/context-options', {formats: customFormats}).then((res) => {
-        assert.deepEqual(res.simple.context()._json, {predicate: 'http://example.org/predicate'})
-
-        simpleFetchLite.defaults.context = null
-      })
-    })
-
-    it('should set the SimpleRDF IRI to the request URL', () => {
-      nock('http://example.org')
-        .get('/iri-request-url')
-        .reply(200, function (url, body) {
-          return [200, '<http://example.org/subject> <http://example.org/predicate> "object" .\n', {
-            'Content-Type': 'application/n-triples'
-          }]
-        })
-
-      const customFormats = {
-        parsers: new rdf.Parsers({
-          'application/n-triples': formats.parsers['application/n-triples']
-        }),
-        serializers: new rdf.Serializers({
-          'application/n-triples': formats.serializers['application/n-triples']
-        })
-      }
-
-      return simpleFetchLite('http://example.org/iri-request-url', {formats: customFormats}).then((res) => {
-        assert.equal(res.simple.iri().toString(), 'http://example.org/iri-request-url')
-      })
-    })
-
-    it('should set the SimpleRDF IRI to the Content-Location header value', () => {
-      nock('http://example.org')
-        .get('/iri-content-location')
-        .reply(200, function (url, body) {
-          return [
-            200,
-            '<http://example.org/subject> <http://example.org/predicate> "object" .\n', {
-              'Content-Location': 'http://example.org/iri',
-              'Content-Type': 'application/n-triples'
-            }
-          ]
-        })
-
-      const customFormats = {
-        parsers: new rdf.Parsers({
-          'application/n-triples': formats.parsers['application/n-triples']
-        }),
-        serializers: new rdf.Serializers({
-          'application/n-triples': formats.serializers['application/n-triples']
-        })
-      }
-
-      return simpleFetchLite('http://example.org/iri-content-location', {formats: customFormats}).then((res) => {
-        assert.equal(res.simple.iri().toString(), 'http://example.org/iri')
-      })
-    })
+  it('should have a defaults object', () => {
+    assert.equal(typeof SimpleFetch.defaults, 'object')
   })
 
-  describe('standard', () => {
-    it('should be a function', () => {
-      assert.equal(typeof simpleFetch, 'function')
-    })
-
-    it('should have a defaults object', () => {
-      assert.equal(typeof simpleFetch.defaults, 'object')
-    })
-
+  describe('static', () => {
     it('should return a Promise object', () => {
-      const result = simpleFetch('http://example.org/', {
+      const result = SimpleFetch.fetch('http://example.org/', {
         rdfFetch: () => {
           return Promise.resolve({})
         }
@@ -256,7 +32,7 @@ describe('simplerdf-fetch', () => {
         return Promise.resolve({})
       }
 
-      return simpleFetch(null, {rdfFetch: touch}).then(() => {
+      return SimpleFetch.fetch(null, {rdfFetch: touch}).then(() => {
         assert.equal(touched, true)
       })
     })
@@ -270,14 +46,14 @@ describe('simplerdf-fetch', () => {
         return Promise.resolve({})
       }
 
-      const defaultRdfFetch = simpleFetch.defaults.rdfFetch
+      const defaultRdfFetch = SimpleFetch.defaults.rdfFetch
 
-      simpleFetch.defaults.rdfFetch = touch
+      SimpleFetch.defaults.rdfFetch = touch
 
-      return simpleFetch().then(() => {
+      return SimpleFetch.fetch().then(() => {
         assert.equal(touched, true)
 
-        simpleFetch.defaults.rdfFetch = defaultRdfFetch
+        SimpleFetch.defaults.rdfFetch = defaultRdfFetch
       })
     })
 
@@ -290,14 +66,14 @@ describe('simplerdf-fetch', () => {
           return [200, '', {'Content-Type': 'application/n-triples'}]
         })
 
-      const simple = new SimpleRDF({predicate: 'http://example.org/predicate'}, 'http://example.org/subject')
+      const simple = new Simple({predicate: 'http://example.org/predicate'}, 'http://example.org/subject')
       simple.predicate = 'object'
 
       const headers = {
         'Content-Type': 'application/n-triples'
       }
 
-      return simpleFetch('http://example.org/send-body', {
+      return SimpleFetch.fetch('http://example.org/send-body', {
         method: 'post',
         headers: headers,
         body: simple
@@ -313,7 +89,7 @@ describe('simplerdf-fetch', () => {
           }]
         })
 
-      return simpleFetch('http://example.org/receive-body').then((res) => {
+      return SimpleFetch.fetch('http://example.org/receive-body').then((res) => {
         const graphString = res.simple.graph().toString().trim()
 
         assert.equal(graphString, '<http://example.org/subject> <http://example.org/predicate> "object" .')
@@ -329,7 +105,7 @@ describe('simplerdf-fetch', () => {
           }]
         })
 
-      return simpleFetch('http://example.org/context-options', {
+      return SimpleFetch.fetch('http://example.org/context-options', {
         context: {
           predicate: 'http://example.org/predicate'
         }
@@ -347,12 +123,98 @@ describe('simplerdf-fetch', () => {
           }]
         })
 
-      simpleFetch.defaults.context = {predicate: 'http://example.org/predicate'}
+      SimpleFetch.defaults.context = {predicate: 'http://example.org/predicate'}
 
-      return simpleFetch('http://example.org/context-options').then((res) => {
+      return SimpleFetch.fetch('http://example.org/context-options').then((res) => {
         assert.deepEqual(res.simple.context()._json, {predicate: 'http://example.org/predicate'})
 
-        simpleFetch.defaults.context = null
+        SimpleFetch.defaults.context = null
+      })
+    })
+
+    it('should support N-Triples', () => {
+      nock('http://example.org')
+        .post('/n-triples')
+        .reply(200, function (url, body) {
+          assert.deepEqual(this.req.headers['content-type'], ['application/n-triples'])
+
+          return [200, body, {'Content-Type': 'application/n-triples'}]
+        })
+
+      const simple = new Simple({predicate: 'http://example.org/predicate'}, 'http://example.org/subject')
+      simple.predicate = 'object'
+
+      const headers = {
+        'Content-Type': 'application/n-triples'
+      }
+
+      return SimpleFetch.fetch('http://example.org/n-triples', {
+        method: 'post',
+        headers: headers,
+        body: simple
+      }).then((res) => {
+        assert.equal(res.headers.get('content-type'), 'application/n-triples')
+
+        assert.equal(
+          res.simple.graph().toCanonical().trim(),
+          '<http://example.org/subject> <http://example.org/predicate> "object" .'
+        )
+      })
+    })
+
+    it('should support JSON-LD', () => {
+      nock('http://example.org')
+        .post('/json-ld')
+        .reply(200, function (url, body) {
+          assert.deepEqual(this.req.headers['content-type'], ['application/ld+json'])
+
+          return [200, body, {'Content-Type': 'application/ld+json'}]
+        })
+
+      const simple = new Simple({predicate: 'http://example.org/predicate'}, 'http://example.org/subject')
+      simple.predicate = 'object'
+
+      const headers = {
+        'Content-Type': 'application/ld+json'
+      }
+
+      return SimpleFetch.fetch('http://example.org/json-ld', {
+        method: 'post',
+        headers: headers,
+        body: simple
+      }).then((res) => {
+        assert.equal(res.headers.get('content-type'), 'application/ld+json')
+
+        assert.equal(
+          res.simple.graph().toCanonical().trim(),
+          '<http://example.org/subject> <http://example.org/predicate> "object" .'
+        )
+      })
+    })
+  })
+
+  describe('instance', () => {
+    it('should be a constructor', () => {
+      assert.equal(typeof SimpleFetch, 'function')
+    })
+
+    it('should use the constructor given in options', () => {
+      const CustomClass = Simple.extend(SimpleFetch)
+      const instance = new CustomClass({}, null, null, {
+        rdfFetch: () => Promise.resolve({
+          headers: {
+            get: () => null
+          },
+          dataset: () => {
+            return Promise.resolve(rdf.dataset())
+          }
+        })
+      })
+
+      return instance.fetch().then((res) => {
+        const plugins = res.simple._plugins.map(p => p.name).sort()
+
+        assert.deepEqual(plugins, ['SimpleFetch', 'SimpleRDF'])
       })
     })
   })
